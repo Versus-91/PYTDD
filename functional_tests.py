ecc4e21fe7b5ae553c09e841ca4a0652547c08a4
Ghader <gharibyan.ghader@gmail.com>
2025-08-31 23:55:41 +0200

    title test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get("http://127.0.0.1:8000/")
 
-assert "Congratulations!" in browser.title
-print("OK")
+
+class NewVisitortest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_new_visitors_website_title(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        self.assertIn("To-Do",self.browser.title) 
+        self.fail("to do is not in title")
+
+
+if __name__ == "__main__":
+    unittest.main()
