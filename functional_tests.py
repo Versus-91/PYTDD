3961647aad6942897e7104f0a963b0fe65cdd007
Ghader <gharibyan.ghader@gmail.com>
2025-09-14 14:37:06 +0200

    updating functional test element ids

--- functional_tests.py
+++ functional_tests.py
@@ -29,7 +29,7 @@ class NewVisitortest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, "id_list_table")
+        table = self.browser.find_element(By.ID, "todo_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
         self.assertTrue(
             any(row.text == "1: Buy peacock feathers" for row in rows))
